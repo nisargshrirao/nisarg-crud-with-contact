8200597a7a73fb31a218778503253b50588561d1
nisargshrirao <nisargshrirao2014@gmail.com>
2022-06-07 13:44:27 +0530

    changes database config

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 // here set the database url 
 const { dbUri } = require('../config');
-
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  autoIndex: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
 const connect = async () => {
   try {
-    const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    };
+    
     const connection = await mongoose.connect(dbUri, options);
     if(connection) console.log('\x1b[32m%s\x1b[0m', 'Database Connected Successfully...');
   } catch (err) {
