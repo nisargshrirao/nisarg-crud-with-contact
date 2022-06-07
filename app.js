c596bd38822b7bd99d6de06834008172d3b316ec
nisargshrirao <nisargshrirao2014@gmail.com>
2022-06-07 13:12:14 +0530

    finding for merge

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const express = require("express");
 const app = express();
 database.connect();
 
-// remove the cors 
+// asfakjdhjdhfkjs
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
